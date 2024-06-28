02ecdf7770f2c55cf9e9bdb9cde0c0d718074144
Björn Steenbeek <bjornsteenbeek@hotmail.nl>
2024-06-28 16:29:46 +0200

    Initial setup

---
+++ Source/unreal_game2/unreal_game2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "unreal_game2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, unreal_game2, "unreal_game2" );
